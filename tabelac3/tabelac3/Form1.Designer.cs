ecadd6dab2a186a756a515560315a88e1f901a21
jhdh26 <jhdh26@gmail.com>
2021-06-22 17:22:56 -0300

    add login and password

--- tabelac3/tabelac3/Form1.Designer.cs
+++ tabelac3/tabelac3/Form1.Designer.cs
@@ -226,6 +226,7 @@ namespace tabelac3
             this.Controls.Add(this.gridtotal);
             this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(11)))), ((int)(((byte)(19)))));
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Admin / E-Commerce";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.gridtotal)).EndInit();
---
+++ tabelac3/tabelac3/Form2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tabelac3
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+
+            Form1 j2 = new Form1();
+            j2.Show();
+        }
+    }
+}
